0935d2d12f601356eed436336354739b4c6c776c
sanjayc2 <chawla_s@yahoo.com>
2021-07-19 19:45:29 -0400

    Add files via upload

---
+++ sample_models.py
@@ -0,0 +1,330 @@
+#from keras import backend as K
+#from keras.models import Model
+#from keras.layers import (BatchNormalization, Conv1D, Dense, Input, 
+#    TimeDistributed, Activation, Bidirectional, SimpleRNN, GRU, LSTM)
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+
+'''def simple_rnn_model(input_dim, output_dim=29):
+    """ Build a recurrent network for speech 
+    """
+    # Main acoustic input
+    input_data = Input(name='the_input', shape=(None, input_dim))
+    # Add recurrent layer
+    simp_rnn = GRU(output_dim, return_sequences=True, 
+                 implementation=2, name='rnn')(input_data)
+    # Add softmax activation layer
+    y_pred = Activation('softmax', name='softmax')(simp_rnn)
+    # Specify the model
+    model = Model(inputs=input_data, outputs=y_pred)
+    model.output_length = lambda x: x
+    print(model.summary())
+    return model
+'''
+
+'''def rnn_model(input_dim, units, activation, output_dim=29):
+    """ Build a recurrent network for speech 
+    """
+    # Main acoustic input
+    input_data = Input(name='the_input', shape=(None, input_dim))
+    # Add recurrent layer
+    simp_rnn = GRU(units, activation=activation,
+        return_sequences=True, implementation=2, name='rnn')(input_data)
+    # TODO: Add batch normalization 
+    bn_rnn = ...
+    # TODO: Add a TimeDistributed(Dense(output_dim)) layer
+    time_dense = ...
+    # Add softmax activation layer
+    y_pred = Activation('softmax', name='softmax')(time_dense)
+    # Specify the model
+    model = Model(inputs=input_data, outputs=y_pred)
+    model.output_length = lambda x: x
+    print(model.summary())
+    return model
+        
+def cnn_rnn_model(input_dim, filters, kernel_size, conv_stride,
+    conv_border_mode, units, output_dim=29):
+    """ Build a recurrent + convolutional network for speech 
+    """
+    # Main acoustic input
+    input_data = Input(name='the_input', shape=(None, input_dim))
+    # Add convolutional layer
+    conv_1d = Conv1D(filters, kernel_size, 
+                     strides=conv_stride, 
+                     padding=conv_border_mode,
+                     activation='relu',
+                     name='conv1d')(input_data)
+    # Add batch normalization
+    bn_cnn = BatchNormalization(name='bn_conv_1d')(conv_1d)
+    # Add a recurrent layer
+    simp_rnn = SimpleRNN(units, activation='relu',
+        return_sequences=True, implementation=2, name='rnn')(bn_cnn)
+    # TODO: Add batch normalization
+    bn_rnn = ...
+    # TODO: Add a TimeDistributed(Dense(output_dim)) layer
+    time_dense = ...
+    # Add softmax activation layer
+    y_pred = Activation('softmax', name='softmax')(time_dense)
+    # Specify the model
+    model = Model(inputs=input_data, outputs=y_pred)
+    model.output_length = lambda x: cnn_output_length(
+        x, kernel_size, conv_border_mode, conv_stride)
+    print(model.summary())
+    return model
+'''
+
+'''
+def cnn_output_length(input_length, filter_size, border_mode, stride,
+                       dilation=1):
+    """ Compute the length of the output sequence after 1D convolution along
+        time. Note that this function is in line with the function used in
+        Convolution1D class from Keras.
+    Params:
+        input_length (int): Length of the input sequence.
+        filter_size (int): Width of the convolution kernel.
+        border_mode (str): Only support `same` or `valid`.
+        stride (int): Stride size used in 1D convolution.
+        dilation (int)
+    """
+    if input_length is None:
+        return None
+    assert border_mode in {'same', 'valid'}
+    dilated_filter_size = filter_size + (filter_size - 1) * (dilation - 1)
+    if border_mode == 'same':
+        output_length = input_length
+    elif border_mode == 'valid':
+        output_length = input_length - dilated_filter_size + 1
+    return (output_length + stride - 1) // stride
+
+class cnn_rnn_model(nn.Module):
+    def __init__(self, input_dim, output_dim=29, rnn_dim=32, num_layers=1, dropout=0.2, \
+                 filters, kernel_size, conv_stride, conv_border_mode, batch_first=True):
+        super(simple_rnn_model, self).__init__()
+
+        model.output_length = lambda x: cnn_output_length(x, kernel_size, conv_border_mode, conv_stride)
+        self.BiGRU = nn.GRU(
+            input_size=input_dim, hidden_size=rnn_dim,
+            num_layers=num_layers, batch_first=batch_first, bidirectional=True)
+        self.layer_norm  = nn.LayerNorm(input_dim)
+        self.dropout     = nn.Dropout(dropout)
+        self.classifier  = nn.Linear(2*rnn_dim, output_dim)
+    
+    def forward(self, x):
+        #print("fwd x shape", x.shape)
+        x = self.layer_norm(x)
+        x = F.gelu(x)
+        x, _ = self.BiGRU(x)
+        x = self.dropout(x)
+        x = self.classifier(x)
+        return x
+'''
+
+'''
+def cnn_output_length(input_length, filter_size, border_mode, stride,
+                       dilation=1):
+    """ Compute the length of the output sequence after 1D convolution along
+        time. Note that this function is in line with the function used in
+        Convolution1D class from Keras.
+    Params:
+        input_length (int): Length of the input sequence.
+        filter_size (int): Width of the convolution kernel.
+        border_mode (str): Only support `same` or `valid`.
+        stride (int): Stride size used in 1D convolution.
+        dilation (int)
+    """
+    if input_length is None:
+        return None
+    assert border_mode in {'same', 'valid'}
+    dilated_filter_size = filter_size + (filter_size - 1) * (dilation - 1)
+    if border_mode == 'same':
+        output_length = input_length
+    elif border_mode == 'valid':
+        output_length = input_length - dilated_filter_size + 1
+    return (output_length + stride - 1) // stride
+
+def deep_rnn_model(input_dim, units, recur_layers, output_dim=29):
+    """ Build a deep recurrent network for speech 
+    """
+    # Main acoustic input
+    input_data = Input(name='the_input', shape=(None, input_dim))
+    # TODO: Add recurrent layers, each with batch normalization
+    ...
+    # TODO: Add a TimeDistributed(Dense(output_dim)) layer
+    time_dense = ...
+    # Add softmax activation layer
+    
+    y_pred = Activation('softmax', name='softmax')(time_dense)
+    # Specify the model
+    model = Model(inputs=input_data, outputs=y_pred)
+    model.output_length = lambda x: x
+    print(model.summary())
+    return model
+
+def bidirectional_rnn_model(input_dim, units, output_dim=29):
+    """ Build a bidirectional recurrent network for speech
+    """
+    # Main acoustic input
+    input_data = Input(name='the_input', shape=(None, input_dim))
+    # TODO: Add bidirectional recurrent layer
+    bidir_rnn = ...
+    # TODO: Add a TimeDistributed(Dense(output_dim)) layer
+    time_dense = ...
+    # Add softmax activation layer
+    y_pred = Activation('softmax', name='softmax')(time_dense)
+    # Specify the model
+    model = Model(inputs=input_data, outputs=y_pred)
+    model.output_length = lambda x: x
+    print(model.summary())
+    return model
+'''
+
+class bidirectional_rnn_model(nn.Module):
+    def __init__(self, input_dim, output_dim=29, rnn_dim=128, hidden_size=128, num_layers=1, dropout=0.2, batch_first=True):
+        super(bidirectional_rnn_model, self).__init__()
+        
+        self.output_length = lambda x: x    # create instance attribute
+        self.BiGRU = nn.GRU(
+            input_size=input_dim, hidden_size=hidden_size,
+            num_layers=1, batch_first=batch_first, bidirectional=True)
+        self.layer_norm  = nn.LayerNorm(input_dim)           # normalization is over number of features
+        self.dropout     = nn.Dropout(dropout)
+        self.classifier  = nn.Linear(2*rnn_dim, output_dim)
+    
+    def forward(self, x):
+        x = x.transpose(2, 3).contiguous().squeeze(1)    # input has extra (channel) dimension, with feature and time dim interchanged
+        #print("fwd x shape", x.shape)
+        x = self.layer_norm(x)
+        x = F.gelu(x)
+        x, _ = self.BiGRU(x)
+        x = self.dropout(x)
+        x = self.classifier(x)
+        return x
+
+class BidirectionalGRU(nn.Module):
+
+    def __init__(self, rnn_dim, hidden_size, dropout, batch_first):
+        super(BidirectionalGRU, self).__init__()
+
+        self.BiGRU = nn.GRU(
+            input_size=rnn_dim, hidden_size=hidden_size,
+            num_layers=1, batch_first=batch_first, bidirectional=True)
+        self.layer_norm = nn.LayerNorm(rnn_dim)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        x = self.layer_norm(x)
+        x = F.gelu(x)
+        x, _ = self.BiGRU(x)
+        x = self.dropout(x)
+        return x
+    
+class CNNLayerNorm(nn.Module):
+    """Layer normalization built for cnns input"""
+    def __init__(self, n_feats):
+        super(CNNLayerNorm, self).__init__()
+        self.layer_norm = nn.LayerNorm(n_feats)
+
+    def forward(self, x):
+        # x (batch, channel, feature, time)
+        x = x.transpose(2, 3).contiguous() # (batch, channel, time, feature)
+        x = self.layer_norm(x)
+        return x.transpose(2, 3).contiguous() # (batch, channel, feature, time) 
+
+
+class ResidualCNN(nn.Module):
+    """Residual CNN inspired by https://arxiv.org/pdf/1603.05027.pdf
+        except with layer norm instead of batch norm
+    """
+    def __init__(self, in_channels, out_channels, n_feats, kernel_size, stride, padding, dilation, dropout):
+        super(ResidualCNN, self).__init__()
+
+        #self.cnn1 = nn.Conv2d(in_channels, out_channels, kernel, stride, padding=kernel//2)
+        #self.cnn2 = nn.Conv2d(out_channels, out_channels, kernel, stride, padding=kernel//2)
+        self.cnn1 = nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding, dilation)
+        self.cnn2 = nn.Conv2d(out_channels, out_channels, kernel_size, stride, padding, dilation)
+        self.dropout1 = nn.Dropout(dropout)
+        self.dropout2 = nn.Dropout(dropout)
+        #print("n_feats", n_feats)
+        self.layer_norm1 = CNNLayerNorm(n_feats)
+        self.layer_norm2 = CNNLayerNorm(n_feats)
+
+    def forward(self, x):
+        residual = x  # (batch, channel, feature, time)
+        x = self.layer_norm1(x)
+        x = F.gelu(x)
+        x = self.dropout1(x)
+        #print("res_cnn fwd cnn1 input shape:", x.shape)
+        x = self.cnn1(x)
+        #print("res_cnn fwd layer_norm2 input shape:", x.shape)
+        x = self.layer_norm2(x)
+        x = F.gelu(x)
+        x = self.dropout2(x)
+        #print("res_cnn fwd cnn2 input shape:", x.shape)
+        x = self.cnn2(x)
+        x += residual
+        return x # (batch, channel, feature, time)
+
+class final_model(nn.Module):
+    
+    def __init__(self, n_cnn_layers, n_rnn_layers, rnn_dim, n_class, n_feats, stride=2, padding=0, dilation=1, dropout=0.1):
+        super(final_model, self).__init__()
+        
+        kernel_size = 3
+        #self.cnn = nn.Conv2d(1, 32, kernel_size, stride=stride, padding=3//2)  # cnn for extracting heirachal features
+        self.cnn = nn.Conv2d(in_channels=1, out_channels=32, kernel_size=3, stride=stride, padding=padding, dilation=dilation)
+
+        #get number of features to pass to res cnn layers.. (based on # output features used in Conv2d above). 
+        #Note, dim=0 for features, 1 for sequence (time)
+        #n_feats = n_feats//2
+        n_feats = int(np.floor((n_feats + 2*padding[0] - dilation[0]*(kernel_size - 1) - 1)// stride[0] + 1))
+        print("num features:", n_feats)
+        # n residual cnn layers with filter size of 32
+        self.rescnn_layers = nn.Sequential(*[
+            ResidualCNN(in_channels=32, out_channels=32, n_feats=n_feats, kernel_size=3, stride=1, 
+                        padding=padding, dilation=dilation, dropout=dropout) 
+            for _ in range(n_cnn_layers)
+        ])
+        self.fully_connected = nn.Linear(n_feats*32, rnn_dim)
+        self.birnn_layers = nn.Sequential(*[
+            BidirectionalGRU(rnn_dim=rnn_dim if i==0 else rnn_dim*2,
+                             hidden_size=rnn_dim, dropout=dropout, batch_first=i==0)
+            for i in range(n_rnn_layers)
+        ])
+        self.classifier = nn.Sequential(
+            nn.Linear(rnn_dim*2, rnn_dim),  # birnn returns rnn_dim*2
+            nn.GELU(),
+            nn.Dropout(dropout),
+            nn.Linear(rnn_dim, n_class)
+        )
+
+    def forward(self, x):
+        x = self.cnn(x)
+        #print("final model fwd rescnn input shape:", x.shape)
+        x = self.rescnn_layers(x)
+        sizes = x.size()
+        x = x.view(sizes[0], sizes[1] * sizes[2], sizes[3])  # (batch, feature, time)
+        x = x.transpose(1, 2) # (batch, time, feature)
+        x = self.fully_connected(x)
+        x = self.birnn_layers(x)
+        x = self.classifier(x)
+        return x
+
+'''
+def final_model():
+    """ Build a deep network for speech 
+    """
+    # Main acoustic input
+    input_data = Input(name='the_input', shape=(None, input_dim))
+    # TODO: Specify the layers in your network
+    ...
+    # TODO: Add softmax activation layer
+    y_pred = ...
+    # Specify the model
+    model = Model(inputs=input_data, outputs=y_pred)
+    # TODO: Specify model.output_length
+    model.output_length = ...
+    print(model.summary())
+    return model
+'''
---
+++ train_utils.py
@@ -0,0 +1,253 @@
+"""
+Defines a functions for training a NN.
+"""
+
+from data_generator import AudioGenerator
+import _pickle as pickle
+
+#from keras import backend as K
+#from keras.models import Model
+#from keras.layers import (Input, Lambda)
+#from keras.optimizers import SGD
+#from keras.callbacks import ModelCheckpoint
+import os
+import torch
+import torch.nn as nn
+import torch.nn.utils as utils
+import torch.optim as optim
+import torch.nn.functional as F
+import torchaudio
+from torch.utils.tensorboard import SummaryWriter
+import numpy as np
+
+'''def ctc_lambda_func(args):
+    y_pred, labels, input_length, label_length = args
+    return K.ctc_batch_cost(labels, y_pred, input_length, label_length)
+
+def add_ctc_loss(input_to_softmax):
+    the_labels = Input(name='the_labels', shape=(None,), dtype='float32')
+    input_lengths = Input(name='input_length', shape=(1,), dtype='int64')
+    label_lengths = Input(name='label_length', shape=(1,), dtype='int64')
+    output_lengths = Lambda(input_to_softmax.output_length)(input_lengths)
+    # CTC loss is implemented in a lambda layer
+    loss_out = Lambda(ctc_lambda_func, output_shape=(1,), name='ctc')(
+        [input_to_softmax.output, the_labels, output_lengths, label_lengths])
+    model = Model(
+        inputs=[input_to_softmax.input, the_labels, input_lengths, label_lengths], 
+        outputs=loss_out)
+    return model
+
+def train_model(input_to_softmax, 
+                pickle_path,
+                save_model_path,
+                train_json='train_corpus.json',
+                valid_json='valid_corpus.json',
+                minibatch_size=20,
+                spectrogram=True,
+                mfcc_dim=13,
+                optimizer=SGD(lr=0.02, decay=1e-6, momentum=0.9, nesterov=True, clipnorm=5),
+                epochs=20,
+                verbose=1,
+                sort_by_duration=False,
+                max_duration=10.0):
+    
+    # create a class instance for obtaining batches of data
+    audio_gen = AudioGenerator(minibatch_size=minibatch_size, 
+        spectrogram=spectrogram, mfcc_dim=mfcc_dim, max_duration=max_duration,
+        sort_by_duration=sort_by_duration)
+    # add the training data to the generator
+    audio_gen.load_train_data(train_json)
+    audio_gen.load_validation_data(valid_json)
+    # calculate steps_per_epoch
+    num_train_examples=len(audio_gen.train_audio_paths)
+    steps_per_epoch = num_train_examples//minibatch_size
+    # calculate validation_steps
+    num_valid_samples = len(audio_gen.valid_audio_paths) 
+    validation_steps = num_valid_samples//minibatch_size
+    
+    # add CTC loss to the NN specified in input_to_softmax
+    model = add_ctc_loss(input_to_softmax)
+
+    # CTC loss is implemented elsewhere, so use a dummy lambda function for the loss
+    model.compile(loss={'ctc': lambda y_true, y_pred: y_pred}, optimizer=optimizer)
+
+    # make results/ directory, if necessary
+    if not os.path.exists('results'):
+        os.makedirs('results')
+
+    # add checkpointer
+    checkpointer = ModelCheckpoint(filepath='results/'+save_model_path, verbose=0)
+
+    # train the model
+    hist = model.fit_generator(generator=audio_gen.next_train(), steps_per_epoch=steps_per_epoch,
+        epochs=epochs, validation_data=audio_gen.next_valid(), validation_steps=validation_steps,
+        callbacks=[checkpointer], verbose=verbose)
+
+    # save model loss
+    with open('results/'+pickle_path, 'wb') as f:
+        pickle.dump(hist.history, f)
+'''
+
+train_audio_transforms = nn.Sequential(
+    #torchaudio.transforms.MelSpectrogram(sample_rate=16000, n_mels=128),
+    torchaudio.transforms.FrequencyMasking(freq_mask_param=30),
+    torchaudio.transforms.TimeMasking(time_mask_param=100)
+)
+#valid_audio_transforms = torchaudio.transforms.MelSpectrogram()
+
+def data_processing(data, data_type="train"):
+    spectrograms = []
+    labels = []
+    input_lengths = []
+    label_lengths = []
+    # data is a dict.. get the waveforms and utterances ndarrays
+    specgrams = data['the_input']                
+    utterances = data['the_labels']
+    for specgram, utterance in zip(specgrams, utterances):
+        if data_type == 'train':
+            spec = torch.Tensor(specgram) #train_audio_transforms(torch.Tensor(specgram))
+        elif data_type == 'valid':
+            spec = torch.Tensor(specgram)
+        else:
+            raise Exception('data_type should be train or valid')
+        spectrograms.append(spec)
+        label = torch.Tensor(utterance)
+        labels.append(label)
+        input_lengths.append(spec.shape[0]//2)
+        label_lengths.append(len(label))
+
+    #print("spectrogram[0] shape:", spectrograms[0].shape)
+    #print("labels[0] shape:", labels[0].shape)
+
+    spectrograms = utils.rnn.pad_sequence(spectrograms, batch_first=True)
+    labels = utils.rnn.pad_sequence(labels, batch_first=True)
+
+    return spectrograms, labels, input_lengths, label_lengths
+
+def train_model_pytorch(model, 
+                        pickle_path,
+                        save_model_path,
+                        optimizer,
+                        train_json='train_corpus.json',
+                        valid_json='valid_corpus.json',
+                        minibatch_size=20,
+                        spectrogram=True,
+                        mfcc_dim=13,
+                        epochs=20,
+                        verbose=1,
+                        sort_by_duration=False,
+                        max_duration=10.0):
+    
+    
+    # make results/ directory, if necessary
+    if not os.path.exists('results'):
+        os.makedirs('results')
+    file_writer = SummaryWriter(log_dir='results/'+save_model_path+'/metrics')
+    use_cuda = torch.cuda.is_available()
+    device = torch.device("cuda" if use_cuda else "cpu")
+    print("device used:", device)
+    model.to(device)
+    
+    start = torch.cuda.Event(enable_timing=True)
+    end = torch.cuda.Event(enable_timing=True)
+    #best_val_acc = 0 # for model check pointing
+    
+    # create a class instance for obtaining batches of data
+    audio_gen = AudioGenerator(minibatch_size=minibatch_size, 
+        spectrogram=spectrogram, mfcc_dim=mfcc_dim, max_duration=max_duration,
+        sort_by_duration=sort_by_duration)
+    # add the training data to the generator
+    audio_gen.load_train_data(train_json)
+    audio_gen.load_validation_data(valid_json)
+    # calculate steps_per_epoch
+    num_train_examples=len(audio_gen.train_audio_paths)
+    steps_per_epoch = num_train_examples//minibatch_size
+    #print("steps per epoch:", steps_per_epoch)    
+    # calculate validation_steps
+    num_valid_samples = len(audio_gen.valid_audio_paths) 
+    validation_steps = num_valid_samples//minibatch_size
+    
+    # add CTC loss to the NN specified in input_to_softmax
+    loss_criterion = nn.CTCLoss(blank=28).to(device)          # blank maps to 28 (see char_map.py)
+    
+    # loop over epochs
+    start.record()
+    train_loss = np.zeros(epochs)
+    valid_loss = np.zeros(epochs)
+    for epoch in range(epochs):
+        # loop over batches to train the model
+        model.train()
+        for batch_idx, _data in enumerate(audio_gen.next_train()): 
+            inputs, outputs = _data
+            spectrograms, labels, input_lengths, label_lengths = data_processing(inputs, "train")
+            # for cnn models, add extra dimension for input channel. Also, transpose features and seq length order
+            spectrograms = spectrograms.unsqueeze(1).transpose(2,3)
+            spectrograms, labels = spectrograms.to(device), labels.to(device)
+            #print("spectrograms shape:", spectrograms.shape)
+
+            optimizer.zero_grad()
+            
+            output = model(spectrograms)  # output shape is (batch, time, n_classes)
+            output = F.log_softmax(output, dim=2)
+            output = output.transpose(0, 1) # change output shape to (time, batch, n_classes) for use in CTC loss calc
+            
+            #print("output shape:", output.shape)
+            #print("labels shape:", labels.shape)
+            #print("input lengths:", input_lengths)
+            #print("label lengths:", label_lengths)
+            
+            loss = loss_criterion(output, labels, input_lengths, label_lengths)
+            loss.backward()
+
+            utils.clip_grad_norm_(model.parameters(), 5)   # gradient clipping 
+            optimizer.step()
+            train_loss[epoch] += loss.item() / steps_per_epoch                     # training loss per batch
+        
+            #if batch id reaches number per epoch, do validaiton and break out of the training for loop
+            #print("batch_idx:", batch_idx)
+            if ((batch_idx+1) % steps_per_epoch == 0):
+                model.eval()
+                with torch.no_grad():
+                    for idx, val_data in enumerate(audio_gen.next_valid()):
+                        inputs, outputs = val_data
+                        spectrograms, labels, input_lengths, label_lengths = data_processing(inputs, "valid")
+                        # for cnn models, add extra dimension for input channel. Also, transpose features and seq length order
+                        spectrograms = spectrograms.unsqueeze(1).transpose(2,3)
+                        spectrograms, labels = spectrograms.to(device), labels.to(device)
+
+                        output = model(spectrograms)  # (batch, time, n_class)
+                        output = F.log_softmax(output, dim=2)
+                        output = output.transpose(0, 1) # (time, batch, n_class)
+
+                        val_loss = loss_criterion(output, labels, input_lengths, label_lengths)
+                        valid_loss[epoch] += val_loss.item() / validation_steps     # validation loss per batch
+                        #print("valid batch idx:", idx)
+                        if ((idx+1) % validation_steps == 0):
+                            break
+                break
+        
+        print("epoch, train and val loss", epoch, train_loss[epoch], valid_loss[epoch])
+        # save train and valid losses for each epoch
+        for epoch in range(epochs):
+            file_writer.add_scalar('Loss/train', train_loss[epoch], epoch)
+            file_writer.add_scalar('Loss/validation', valid_loss[epoch], epoch)
+
+    # after all epochs, save model loss using pytorch save
+    # add checkpoint
+    filepath='results/'+save_model_path+'.pt'
+    torch.save({
+            'epoch': epoch,
+            'model_state_dict': model.state_dict(),
+            'optimizer_state_dict': optimizer.state_dict(),
+            'loss': train_loss,
+            'valid_loss': valid_loss,
+            }, filepath)
+    
+    end.record()
+    torch.cuda.synchronize()
+    print("total time taken:", start.elapsed_time(end))
+
+    with open('results/'+pickle_path, 'wb') as f:
+        torch.save({'loss': train_loss,
+                   'val_loss': valid_loss}, f)
+                   
